5bd6eb5449a0d440dda1dc6d10e9127d4c2ba1d9
takoi <takoiwebile@gmail.com>
2021-09-17 10:03:07 +0200

    added basic setup and test route

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-setInterval(() => console.log(new Date().getTime()), 500);
+const express = require( 'express');
+
+const app = express();
+const port = 4000;
+
+app.get('/test', (req, res) => {
+    res
+        .append('Access-Control-Allow-Origin', '*')
+        .send({text:'Hello World!'});
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
