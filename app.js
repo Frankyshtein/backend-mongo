fbb5314da26a936f7a289e36c878693897418c43
takoi <takoiwebile@gmail.com>
2021-09-18 20:19:58 +0200

    added basic structure

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require( 'express');
 
+const testRoutes = require('./routes/test');
+
 const app = express();
 const port = 4000;
 
-app.get('/test', (req, res) => {
-    res
-        .append('Access-Control-Allow-Origin', '*')
-        .send({text:'Hello World!'});
+app.use((req, res, next) => {
+    console.log(req.method,req.path);
+
+    res.append('Access-Control-Allow-Origin', '*');
+    next();
 })
+
+app.use(testRoutes);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
